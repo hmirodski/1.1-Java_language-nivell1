25f0851a1571e48a844d23ec8a5aef11dab32fe9
Horacio Miroddi <48515132+hmirodski@users.noreply.github.com>
2025-11-04 11:00:52 +0100

    Main class completed
    Created Instrument objects of each type in Main class.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Instruments flute = new Winds("flute", 300);
+        flute.Play();
+
+        Instruments Strings = new Strings("Cello", 100);
+        Strings.Play();
+
+        Instruments Percussion = new Percussion("Drums", 200);
+        Percussion.Play();
     }
 
 }
